6e472bc16b979ec0d66390c6cfcbc5b4aeca63bb
Office Laptop <pg@gmail.com>
2023-07-01 16:52:00 +0600

    update code from office

--- src/test/java/Git_pac/Class_Two.java
+++ src/test/java/Git_pac/Class_Two.java
@@ -6,5 +6,6 @@ public class Class_Two {
         System.out.println(" Your Class_Two Ok ! ");
         System.out.println(" One Time Updating ");
         System.out.println(" Updating code from Office Laptop ");
+        System.out.println(" Again updating my code for test ");
     }
 }
