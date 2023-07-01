f1f6e338520d6ab199296ecdba4416a9d805f50a
purno-ghosh <pg.purnoghosh@gmail.com>
2023-07-01 16:34:27 +0600

    Conniting

--- src/test/java/Git_pac/Class_Two.java
+++ src/test/java/Git_pac/Class_Two.java
@@ -4,5 +4,6 @@ public class Class_Two {
 
     public static void main(String[] args) {
         System.out.println(" Your Class_Two Ok ! ");
+        System.out.println(" One Time Updating ");
     }
 }
